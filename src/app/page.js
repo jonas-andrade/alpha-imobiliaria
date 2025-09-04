2fe0f8ec76d434e0bdda8bd27ead011625eb856a
jonas medeiros andrade <jonas6116m@ogmai.com>
2025-09-04 16:16:08 -0300

    page main - home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,14 @@
 "use client";
-import Image from 'next/image';
-
-
+import HeroCarousel from "../components/layout/HeroCarousel";
+import LeadSection from "../components/layout/LeadSection";
+import FeaturedSales from "../components/layout/FeaturedSales";
 
 export default function Home() {
-
   return (
- <div className="relative w-full h-screen bg-gray-100">
-      <Image
-        src="/images/hero.jpg"
-        alt="Casa à venda"
-        fill
-        className="object-cover"
-        priority
-      />
-      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-white">Todos os Imóveis</h1>
-      </div>
+    <div>
+      <HeroCarousel />
+      <LeadSection />
+      <FeaturedSales />
     </div>
   );
 }
